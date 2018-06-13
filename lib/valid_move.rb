cf281c78740242bd271b2bc8e4f3432dadb4398b
Nate Miller <natekmiller96@gmail.com>
2018-06-13 16:35:41 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,12 +1,10 @@
 def valid_move?(board,index)
-  if index.between(0,8) && position_taken?(board,index)
+  if index.between?(0,8) && position_taken?(board,index)
     return true
   else
     return false
   end
 end
-
-
 def position_taken?(board,index)
   if board[index] == "X" || board[index] == "O"
     return true
